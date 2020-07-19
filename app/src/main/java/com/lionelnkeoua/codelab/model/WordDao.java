50a21023419f9f23fef4aed5869ee36da48c466c
lionel <nkeoualionel@gmail.com>
2020-07-19 16:26:48 +0200

    implementation of query for our table

---
+++ app/src/main/java/com/lionelnkeoua/codelab/model/WordDao.java
@@ -0,0 +1,21 @@
+package com.lionelnkeoua.codelab.model;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("select * from table_word order by word asc")
+    LiveData<List<Word>> getAllWord();
+
+    @Query("delete  from table_word")
+    void deleteAll();
+
+}
