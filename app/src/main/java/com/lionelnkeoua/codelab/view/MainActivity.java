bc9c9aa4a2f1b0a931f603248ab4f7f0d2266b51
lionel <nkeoualionel@gmail.com>
2020-07-19 15:45:31 +0200

    initial commit

---
+++ app/src/main/java/com/lionelnkeoua/codelab/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lionelnkeoua.codelab.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lionelnkeoua.codelab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
