f5069fad99cfa3d5eb1345b773fd4232672058e4
lionel <nkeoualionel@gmail.com>
2020-07-19 19:14:36 +0200

    added viewModel

--- app/src/main/java/com/lionelnkeoua/codelab/view/MainActivity.java
+++ app/src/main/java/com/lionelnkeoua/codelab/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lionelnkeoua.codelab.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,13 +9,18 @@ import android.os.Bundle;
 
 import com.lionelnkeoua.codelab.R;
 import com.lionelnkeoua.codelab.adapters.WordListAdapter;
+import com.lionelnkeoua.codelab.viewmodel.WordViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final WordListAdapter adapter = new WordListAdapter(this);
