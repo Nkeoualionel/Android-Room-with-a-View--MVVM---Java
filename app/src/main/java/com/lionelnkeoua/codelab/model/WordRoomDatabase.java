809511d505a47dc50f166f2762e8556bf168e215
lionel <nkeoualionel@gmail.com>
2020-07-19 16:40:55 +0200

    create database

---
+++ app/src/main/java/com/lionelnkeoua/codelab/model/WordRoomDatabase.java
@@ -0,0 +1,33 @@
+package com.lionelnkeoua.codelab.model;
+
+import android.content.Context;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static volatile WordRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREAD = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREAD);
+
+    static WordRoomDatabase getDatabase(final Context context){
+
+        if (INSTANCE == null){
+                synchronized (WordRoomDatabase.class){
+                        if(INSTANCE == null){
+                            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    WordRoomDatabase.class, "word_database")
+                                    .build();
+                        }
+                }
+        }
+
+        return INSTANCE;
+    }
+}
