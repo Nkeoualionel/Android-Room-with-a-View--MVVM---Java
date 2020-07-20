8ee3b7f1c949fd10b85ec693bc33511556548a78
lionel <nkeoualionel@gmail.com>
2020-07-20 05:19:00 +0200

    debug dao

--- app/src/main/java/com/lionelnkeoua/codelab/model/WordDao.java
+++ app/src/main/java/com/lionelnkeoua/codelab/model/WordDao.java
@@ -3,10 +3,12 @@ package com.lionelnkeoua.codelab.model;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
+@Dao
 public interface WordDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
